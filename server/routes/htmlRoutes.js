b635383db31526e70c5102ffa38c4c3b87474e8b
Bunix25 <116853300+Bunix25@users.noreply.github.com>
2023-04-19 12:12:39 -0400

    Update htmlRoutes.js

--- server/routes/htmlRoutes.js
+++ server/routes/htmlRoutes.js
@@ -1,6 +1,10 @@
+// Import the Node.js path module
 const path = require('path');
 
+// Export a function that takes an Express app object as a parameter
 module.exports = (app) =>
+  // Handle GET requests to the root URL
   app.get('/', (req, res) =>
+    // Send the index.html file as a response
     res.sendFile(path.join(__dirname, '../client/dist/index.html'))
   );

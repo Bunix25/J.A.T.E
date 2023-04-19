4b544f6c88bd4242903b0b9eb77d47160605900e
Bunix25 <116853300+Bunix25@users.noreply.github.com>
2023-04-19 12:13:23 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,12 +1,21 @@
+// Import the Express library
 const express = require('express');
 
+// Create a new Express app
 const app = express();
+
+// Set the port number to listen on
 const PORT = process.env.PORT || 3000;
 
+// Serve static files from the ../client/dist directory
 app.use(express.static('../client/dist'));
+
+// Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Require the htmlRoutes module and pass the app object as a parameter
 require('./routes/htmlRoutes')(app);
 
+// Start listening for requests on the specified port
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));

c73d352087327be3f7fa8d5844facab3a30110db
Bunix25 <116853300+Bunix25@users.noreply.github.com>
2023-04-19 12:40:48 -0400

    Update database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,7 +12,7 @@ const initdb = async () =>
     },
   });
 
-
+// TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   // Database and version
   const jateDB = await openDB("jate", 1);
@@ -27,7 +27,7 @@ export const putDb = async (content) => {
   console.log("🚀 - data saved to the database", result);
 };
 
-
+// TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   // Database and version
   const jateDB = await openDB("jate", 1);
@@ -44,3 +44,4 @@ export const getDb = async () => {
 };
 
 initdb();
+
